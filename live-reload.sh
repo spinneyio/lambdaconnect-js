2e7b535b314462f1b576afe56d0cc74ecaa087c9
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2020-03-23 14:24:56 +0100

    Added live-reload script
    Signed-off-by: Szymon Piechaczek <piechaczek.szymon@gmail.com>

---
+++ live-reload.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function build {
+  yarn prepare
+}
+
+function sync {
+  rsync -av --delete --exclude 'node_modules/' --exclude 'src/' --exclude '.git/' ./ ../lambdaconnect-react-demo/node_modules/lambdaconnect-js
+}
+
+build
+sync
+
+while inotifywait -q -r --exclude '(\.git/|\.idea/|\.build/|build/)' -e modify,create,delete,move,move_self .; do
+  build
+  sync
+done
