c25292bfa652125d2d3be7ea94e421ffa68a13e6
srsly4 <piechaczek.szymon@gmail.com>
2020-06-04 14:14:07 +0200

    Exported ViewModel
    Signed-off-by: srsly4 <piechaczek.szymon@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // @flow
 import Database from './database';
+import ViewModel from './view-model';
 import useViewModel from './use-view-model';
 
 export {
   Database,
+  ViewModel,
   useViewModel,
 }
